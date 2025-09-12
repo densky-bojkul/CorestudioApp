85868a4bcdf4fcae9da143d10ef1d5b7060fd1be
Densky <densky.bojkul@gmail.com>
2025-09-13 00:59:47 +0700

    fixing splash

--- app/src/main/java/com/nunucore/corestudio/SplashActivity.java
+++ app/src/main/java/com/nunucore/corestudio/SplashActivity.java
@@ -2,25 +2,20 @@ package com.nunucore.corestudio;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DELAY = 2000; // 2 detik
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Aktifkan SplashScreen API
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash); // ambil dari layout XML
 
-        // Delay sebelum masuk ke MainActivity
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, SPLASH_DELAY);
+        // Pindah ke MainActivity setelah selesai
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
+
