b0e72c80eb3315392a7e13496b01c01c36e1cbe5
Densky <densky.bojkul@gmail.com>
2025-09-13 03:29:22 +0700

    rubah splash screen

--- app/src/main/java/com/nunucore/corestudio/SplashActivity.java
+++ app/src/main/java/com/nunucore/corestudio/SplashActivity.java
@@ -1,19 +1,13 @@
-package com.nunucore.corestudio;
-
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
-
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Aktifkan SplashScreen API
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        // Langsung redirect ke MainActivity
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2000); // 2 detik splash
     }
 }
